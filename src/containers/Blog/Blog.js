2ed1adc4cfa5502e89ea103eda728c1c341d19d2
tejesh <tejeshmuralikrishna@gmail.com>
2020-06-28 22:15:40 +0530

    Using switch to load a single route We can make sure only one route loads, by wrapping the routes inside switch component.     import { Route, NavLink, Switch } from 'react-router-dom';     <Switch>       <Route path="/" exact component={Posts} />       <Route path="/new-post" component={NewPost} />       <Route path="/:id" exact component={FullPost} />     </Switch> React router only executes the first route which matches the route.
    We can also implement some flexible patterns as below
        <Route path="/" exact component={Posts} />
        <Switch>
            <Route path="/new-post" component={NewPost} />
            <Route path="/:id" exact component={FullPost} />
        </Switch>
        <Route path="/post" exact component={Posts} />

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 // import axios from 'axios';
 
 // import Post from '../../components/Post/Post';
@@ -29,10 +29,12 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" exact render={() => <h1>Home 2</h1>} /> */}
 
-                <Route path="/" exact component={Posts} /> 
-                <Route path="/new-post" component={NewPost} /> 
-                <Route path="/:id" exact component={FullPost} /> 
-
+                <Switch>
+                    <Route path="/" exact component={Posts} /> 
+                    <Route path="/new-post" component={NewPost} /> 
+                    <Route path="/:id" exact component={FullPost} /> 
+                </Switch>
+                
                 {/* <Posts /> */}
                 {/* <section>
                     <FullPost id={this.state.selectedPostId} />
