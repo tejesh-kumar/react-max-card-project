2cc6c8e5d8e8b204a078423f63aa5736ddcd8bfd
tejesh <tejeshmuralikrishna@gmail.com>
2020-06-28 14:17:02 +0530

    Preparing & restructuring the project for routing Converting posts, full post, new post to containers to contain the state Perform routing to show that particular page for a specific route.

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from '../../../axios';
 
-const Posts = props => (
-    <section className="Posts">
-        {posts}
-    </section>
-);
+import Post from '../../../components/Post/Post';
+import './Posts.css';
+
+class Posts extends Component {
+    state = {
+        posts: [],
+    }
+
+    componentDidMount () {
+        axios.get('/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                return {
+                    ...post,
+                    author: 'tejesh'
+                }
+            });
+                this.setState({posts: updatedPosts});
+                // console.log(response);
+            })
+            .catch(err => {
+                console.log(err);
+                // this.setState({error: true});
+            });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
+    render () {
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>
+
+        if(!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post key={post.id} title={post.title} author={post.author} clicked={() => {this.postSelectedHandler(post.id)}} />
+            })
+        }
+
+        return (
+            <section className="Posts">
+                {posts}
+            </section>
+        );
+    }
+}
 
 export default Posts;
