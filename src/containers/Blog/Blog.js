ca4d8b66abf4fd1876926bceed9ed527d7fab843
tejesh <tejeshmuralikrishna@gmail.com>
2020-06-28 14:47:36 +0530

    Rendering components for routes <Route path="/" exact component={Posts}  /> component attribute holds a reference to the function or class component that should be rendered for the route.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -22,8 +22,10 @@ class Blog extends Component {
                     </nav>
                 </header>
 
-                <Route path="/" exact render={() => <h1>Home</h1>} />
-                <Route path="/" exact render={() => <h1>Home 2</h1>} />
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" exact render={() => <h1>Home 2</h1>} /> */}
+
+                <Route path="/" exact component={Posts}  />
 
                 {/* <Posts /> */}
                 {/* <section>
