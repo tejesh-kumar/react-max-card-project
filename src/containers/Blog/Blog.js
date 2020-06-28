8f2cfbb83847b21f25c20cc69fa78ab337e4ef05
tejesh <tejeshmuralikrishna@gmail.com>
2020-06-28 20:08:08 +0530

    Styling the active route Adding a special css class to style the link or route that is active We can use Navlink component(similar to Link which contains some extra props to style the active page) An exact attribute to be added otherwise react will treat those links also active if it matches part of active link for comparision. eg:- home => '/' & new-post => '/new-post', since the path for home is part of other route-link as well so, if '/new-post' is active, react considers both 'home' & 'new-post' both to be active.
    To add our own class for active links by adding activeClassName attribute on the Navlink
        <li><NavLink exact to="/" activeClassName="my-active">Home</NavLink></li>
    Now, the default active className can be overridden by the "my-active" className for home link.
    
    We can also set active style instead
         <li><NavLink exact to="/" activeStyle={{color: '#fa923f', textDecoration: 'underline'}}>Home</NavLink></li>

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 // import axios from 'axios';
 
 // import Post from '../../components/Post/Post';
@@ -16,12 +16,12 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
-                                pathname: this.props.match.url + '/new-post',
+                            <li><NavLink exact to="/" activeClassName="my-active" activeStyle={{color: '#fa923f', textDecoration: 'underline'}}>Home</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
                                 hash: '#submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
